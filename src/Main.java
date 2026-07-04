c5c82371e2bdc55b5fa07e372547b91a6b602fbb
anirudhcheeti <anirudhcheeti.dev@gmail.com>
2026-07-04 10:52:07 +0530

    Basic Recursion

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,16 @@ public class Main {
     public static void main(String[] args) {
         message();
     }
-
     static void message() {
         System.out.println("Hello World!");
         System.out.println("Hello World! Once Again");
         message1();
     }
-
     static void message1() {
         System.out.println("Hello World!");
         System.out.println("Hello World! Once Again");
         message2();
     }
-
     static void message2() {
         System.out.println("Hello World!");
         System.out.println("Hello World! Once Again");
