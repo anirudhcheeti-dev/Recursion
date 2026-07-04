34ebf673ce065552bdbd7061aa1f8c6d0753dbc0
anirudhcheeti <anirudhcheeti.dev@gmail.com>
2026-07-04 10:50:43 +0530

    Basic Recursion

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    public static void main(String[] args) {
+        message();
+    }
+
+    static void message() {
+        System.out.println("Hello World!");
+        System.out.println("Hello World! Once Again");
+        message1();
+    }
+
+    static void message1() {
+        System.out.println("Hello World!");
+        System.out.println("Hello World! Once Again");
+        message2();
+    }
+
+    static void message2() {
+        System.out.println("Hello World!");
+        System.out.println("Hello World! Once Again");
+    }
 }
