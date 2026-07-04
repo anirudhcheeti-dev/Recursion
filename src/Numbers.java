71323a40ecb304669f514503d221543cdb3a25ae
anirudhcheeti <anirudhcheeti.dev@gmail.com>
2026-07-04 11:04:29 +0530

    Numbers Recursion result

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,2 +1,26 @@
 public class Numbers {
+
+    static void main() {
+        //Print numbers from 1 to 5.
+        numbers1(1);
+    }
+    static void numbers1(int n) {
+        System.out.println(n);
+        numbers2(2);
+    }
+    static void numbers2(int n) {
+        System.out.println(n);
+        numbers3(3);
+    }
+    static void numbers3(int n) {
+        System.out.println(n);
+        numbers4(4);
+    }
+    static void numbers4(int n) {
+        System.out.println(n);
+        numbers5(5);
+    }
+    static void numbers5(int n) {
+        System.out.println(n);
+    }
 }
