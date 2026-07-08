79783a140a3ed655b454c075ed3b44fe83e239c0
anirudhcheeti <anirudhcheeti.dev@gmail.com>
2026-07-08 23:02:02 +0530

    1ton

--- src/Basics/print1ton.java
+++ src/Basics/print1ton.java
@@ -6,12 +6,14 @@ public class print1ton {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        print(n);
     }
-    public static int print(int n){
-        if(n==1){
-            System.out.println(n);
-            return n;
+    public static void print(int n){
+        if(n<1){
+            //System.out.println(n);
+            return;
         }
-        return print(n-1);
+        print(n-1);
+        System.out.println(n);
     }
 }
