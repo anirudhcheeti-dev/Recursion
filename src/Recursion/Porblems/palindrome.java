3ceb6e0706e30a62c271faaa8ade1b9e57e59676
anirudhcheeti <anirudhcheeti.dev@gmail.com>
2026-07-30 21:14:23 +0530

    palindrome

--- src/Recursion/Porblems/palindrome.java
+++ src/Recursion/Porblems/palindrome.java
@@ -1,45 +1,38 @@
 package Recursion.Porblems;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class palindrome {
     public static void main(String[] args) {
-        String s = "gvffvg";
+        String s = "gvfgfvg";
         helper(s);
         System.out.println(helper(s));
         return;
     }
 
     public static String helper(String s) {
-//        if (s.length() <=3) return s;
-//        int[] len=new int[26];
-//            for(int i=0;i<len.length/2;i++){
-//                len[s.charAt(i)-'a']++;
-//            }
-//            for(int i=0;i<26;i++){
-//                if(len[i]!=0){}
-//            }
-
-
-        if (s.length() <= 3) return s;
-        char[] arr = s.toCharArray();
-        int i = 1;
-        int j = arr.length - 2;
-        while (i < j) {
-            char ch = arr[i];
-            char ch1=arr[i-1];
-            if (ch < ch1) {
-                char temp = arr[i];
-                arr[i] = arr[i - 1];
-                arr[i - 1] = temp;
-                char temp2 = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp2;
+        ArrayList<Character> list = new ArrayList<>();
+            for (int i = 0; i < s.length() / 2; i++) {
+                list.add(s.charAt(i));
             }
-            i++;
-            j--;
+            Collections.sort(list);
+            ArrayList<Character> list2 = new ArrayList<>(list);
+            Collections.reverse(list2);
+
+            StringBuilder sb = new StringBuilder();
+
+        for (char ch : list) {
+            sb.append(ch);
         }
-        return new String(arr);
+        if(s.length()%2!=0){
+            sb.append(s.charAt(s.length()/2));
+        }
+        for(char ch : list2){
+            sb.append(ch);
+        }
+        return sb.toString();
     }
 }
 
